b47208e8359e3774efe629a3ca34872faacbbf38
Aleksey <rusal@bk.ru>
2022-02-13 23:37:13 +0300

    stego

---
+++ steg_sample.py
@@ -0,0 +1,9 @@
+﻿#encoding
+steg = LSBSteg(cv2.imread("my_image.png"))
+img_encoded = steg.encode_text("my message")
+cv2.imwrite("my_new_image.png", img_encoded)
+
+#decoding
+im = cv2.imread("my_new_image.png")
+steg = LSBSteg(im)
+print("Text value:",steg.decode_text())
